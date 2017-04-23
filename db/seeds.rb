8d18abe0c2e9c2e3c683a0fd230bfdb9ae6c2018
Alberto Rocha <betogrun@gmail.com>
2017-04-23 01:01:57 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 u1 = User.create email: 'fake@user.com', password: '12345678'
 u2 = User.create email: 'fake2@user.com', password: '12345678'
 
-n1 = Note.create user: u1, title: 'Bootcamp 2 OneBitCode', body: 'Vai ser incrível :)'
-n2 = Note.create user: u2, title: 'Aprender Rails', body: 'De maneira fácil :)'
+n1 = Note.create user: u1, title: 'My first note!', body: 'This app is amazinhg!'
+n2 = Note.create user: u2, title: 'Another Note', body: 'It is easy to add a new note'
 
 t1 = Tag.create title: 'Rails', user: u1
 t2 = Tag.create title: 'React', user: u2
